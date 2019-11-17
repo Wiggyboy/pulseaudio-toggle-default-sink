c20c4ace5af5864e1f75f0bc57866b799e50e5b7
Wiggyboy <Wiggy0boy@gmail.com>
2019-11-17 14:27:02 +0100

    Create audio-sinks data array

---
+++ audio-sinks.py
@@ -0,0 +1,12 @@
+# 
+# Copyright 2019 (C) Wiggy boy <Lindholm>
+# (formally known as Osvald Lindholm)
+#
+
+# This file we specifies which audio sinks to toggle through
+# The order of the sinks will the same as the toggle order
+
+
+audio_sinks = [
+    # Add yours sinks here
+]
