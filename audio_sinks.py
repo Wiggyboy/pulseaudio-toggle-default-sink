77df9d8a478f8222378c865639b9b27e5d246113
Wiggyboy <Wiggy0boy@gmail.com>
2019-11-17 19:48:40 +0100

    Create verification of audio sinks to toggle

--- audio_sinks.py
+++ audio_sinks.py
@@ -3,9 +3,35 @@
 # (formally known as Osvald Lindholm)
 #
 
-# This file we specifies which audio sinks to toggle through.
+# This file we specifies which audio sinks (by name) to toggle through.
 # The order of the sinks will the same as the toggle order.
 
 audio_sinks = [
-    # Add yours sinks here
+    # Edit/Add yours sinks here
+    #
+    # Example of two possible audio sink:
+    # 'alsa_output.pci-0000_00_1f.3.analog-stereo',
+    # 'alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3',
 ]
+
+# Do not edit below this line
+
+# Verify audio_sinks is valid
+if (len(audio_sinks) == 0):
+    from subprocess import getoutput
+    from textwrap import dedent
+
+    get_available_audio_sinks_cmd = "pactl list sinks short | grep -o -P '(?<=[0-9]\\s)[A-z0-9\\.\\-\\_]+'"
+    available_audio_sinks = list(getoutput(get_available_audio_sinks_cmd).split())
+
+    print(dedent("""\
+        audio_sinks cannot be empty.
+
+        You probably missed to configure your audio sinks.
+        Edit audio_sinks.py to fill in the audio_sinks
+        you would like to be able to toggle through.
+
+        Detected possible audio sinks are:
+    """) + "\n".join(available_audio_sinks)
+    )
+    exit(0)
